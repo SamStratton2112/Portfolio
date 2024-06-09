62adf96dee8119656fc6a9ebbaf248090b6c35b2
SamStratton2112 <109560024+SamStratton2112@users.noreply.github.com>
2024-06-09 12:15:31 -0700

    Career Coach Updates
    - Update about me
    - add completed projects link
    - add tech stack at bottom of about me

--- src/About.js
+++ src/About.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import './About.css'
 import headshot from './static/headshot.png'
 
@@ -17,20 +17,32 @@ const About = () =>{
                 </div>
                 <div className="About-paragraphs ">
                     <p>
-                    I am a determined professional with a diverse background and a deep appreciation for logical reasoning. Originally aspiring to be a helicopter pilot, I shifted my focus to technology after witnessing the outdated software plaguing the aviation industry.
+                    I am a passionate and driven professional with a unique journey from aspiring helicopter pilot to dedicated software engineer. My transition into technology was sparked by a desire to modernize the outdated software systems I encountered in aviation.
                     </p>
                     <p>
-                    Discovering a passion for software development, I pursued it further and now have over a year of experience in full stack web development. I've worked on 3 major projects using various languages, including JavaScript and Python, along with libraries and frameworks like React, Express, and Flask as well as Node.js. From portfolios to online stores to RESTful API development, my full stack skill set is ever expanding.  
+                    Pursuing this newfound passion, I completed Springboard's software engineering course, where I developed a robust skill set in full stack web development. With over a year of hands-on experience, I have successfully delivered three major projects. My technical expertise spans languages such as JavaScript and Python, and I am proficient with libraries and frameworks including React, Express, Flask, and Node.js among others.
                     </p>
                     <p>
-                    The transition from the cockpit to the command line has been an exhilarating journey. I am eager to contribute my skills to new projects and continue learning within a collaborative environments.
+                    My project portfolio includes developing professional portfolios, building online stores, and designing RESTful APIs, all of which showcase my expanding capabilities in the full stack domain. This journey from the cockpit to the command line has been thrilling, and I am excited to bring my skills and enthusiasm to innovative projects.
                     </p>
-                    <div className="links">
+                    <p>
+                    I thrive in collaborative environments and am eager to contribute to impactful projects while continuously advancing my knowledge and skills.
+                    </p>
+                    <p>
+                    <b>Front-End:</b> JavaScript | React.js | jQuery | HTML | CSS | Bootstrap | Jinja | Jasmine
+                    <br/>
+                    <b>Back-End:</b> Python | Flask | Node.js | Express.js | PostgreSQL | SQLAlchemy
+                    <br/>
+                    <b>Testing:</b> Jest | Jasmin | React Testing Library    
+                    </p>
+                    
+                </div>
+            </div>
+            <div className="links">
+                        <button className="btn"><NavLink className="link" to='/projects/complete'>Completed Projects</NavLink></button>
                         <button className="btn"><Link className="link" to='https://github.com/SamStratton2112'>GitHub</Link></button>
                         <button className="btn"><Link className="link" to='https://www.linkedin.com/in/samantha-a-stratton/'>LinkedIn</Link></button>
                     </div>
-                </div>
-            </div>
         </div>
     )
 }
