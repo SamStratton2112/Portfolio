8aa00f81b99ef420ebb32c23d675a8395df30ffc
SamStratton2112 <109560024+SamStratton2112@users.noreply.github.com>
2024-06-21 13:15:38 -0700

    fix links

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,20 +2,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './NavBar.css'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 
 
 const NavBar = () =>{
     return(
         <Navbar collapseOnSelect expand="lg" className="NavBar">
-          <Navbar.Brand href="/" className='me-auto nav-name'>Samantha Stratton</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/" className='me-auto nav-name'>Samantha Stratton</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-                <Nav.Link className='NavBar-link' href="/projects/complete">Software Engineering Projects</Nav.Link>
-                <Nav.Link className='NavBar-link' href="/projects/current">Current Projects</Nav.Link>
-                <Nav.Link className='NavBar-link' href="/resume">Resume</Nav.Link>
-                <Nav.Link className='NavBar-link' href="/certifications">Certifications</Nav.Link>
+                <Nav.Link className='NavBar-link' as={Link} to="/projects/complete">Software Engineering Projects</Nav.Link>
+                <Nav.Link className='NavBar-link' as={Link} to="/projects/current">Current Projects</Nav.Link>
+                <Nav.Link className='NavBar-link' as={Link} to="/resume">Resume</Nav.Link>
+                <Nav.Link className='NavBar-link' as={Link} to="/certifications">Certifications</Nav.Link>
             </Nav>
           </Navbar.Collapse>
       </Navbar>
